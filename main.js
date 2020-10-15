25ef7d0e79a42784a24653b5795d4e419f08f507
Restitutor <Restitutor@users.noreply.github.com>
2020-10-15 19:35:24 -0400

    Refactor

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js');
-const { Bot } = require('./lib/bot');
+const {Bot} = require('./lib/bot');
 
 const config = require('./config.json');
 const pixbot = new Bot(config);
